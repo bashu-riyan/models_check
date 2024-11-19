4cc43b983ef7a78f15a6fe5dae519c242c4fcfcf
bashu-riyan <mdbasheerb252@gmail.com>
2024-11-19 21:46:05 +0530

    basheer commit

---
+++ ItemNotfound.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemNotfound extends Model
+{
+    protected $itemsearched = ['item_notfound'];
+}
---
+++ ModuleZone.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ModuleZone extends Pivot
+{
+    use HasFactory;
+
+    protected $casts = [
+        'id'=>'integer',
+        'module_id'=>'integer',
+        'zone_id'=>'integer',
+        'initial_fix_kms'=>'float',
+        'initial_fix_km_charge'=>'float',
+        'per_km_shipping_charge'=>'float',
+        'minimum_shipping_charge'=>'float',
+        'maximum_shipping_charge'=>'float',
+        'maximum_cod_order_amount'=>'float',
+    ];
+}
